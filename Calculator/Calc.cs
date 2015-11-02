b7023145c2a3275ba5257296628dcc24d50cb368
falehOussama <falehoussama.ch@gmail.com>
2015-11-02 19:43:36 +0100

    failing-calculator resolved

--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -1,10 +1,23 @@
 ﻿namespace Calculator
 {
-    public class Calc
+    #region Class Calc --------------------------------------------------------
+    /// <summary>
+    /// Class modeling a set of calculation methods.
+    /// </summary>
+    public static class Calc
     {
+        #region Methods -------------------------------------------------------
+        /// <summary>
+        /// Add : Method that allows the addition of two integers
+        /// </summary>
+        /// <param name="op1">First operand</param>
+        /// <param name="op2">Second operand</param>
+        /// <returns>Returns the sum of the two operands parameters</returns>
         public static int Add(int op1, int op2)
         {
-            return op1 + op2
+            return op1 + op2;
         }
+        #endregion
     }
+    #endregion
 }
